ac50afca70e48fc3b6d83139065acd1b476b69bf
cleitonsilva14 <cleiton18ads@gmail.com>
2026-05-22 16:49:59 -0300

    Ajustes imports não usados

--- src/main/java/io/dev/byteacademy/controller/CourseController.java
+++ src/main/java/io/dev/byteacademy/controller/CourseController.java
@@ -1,6 +1,5 @@
 package io.dev.byteacademy.controller;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
