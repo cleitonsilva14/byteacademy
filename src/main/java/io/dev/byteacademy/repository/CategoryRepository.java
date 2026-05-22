07b368fd1a29da6943d7082dfaaa7bc659660e0b
cleitonsilva14 <cleiton18ads@gmail.com>
2026-05-22 17:01:51 -0300

    Implementando findByNameIn

--- src/main/java/io/dev/byteacademy/repository/CategoryRepository.java
+++ src/main/java/io/dev/byteacademy/repository/CategoryRepository.java
@@ -1,5 +1,8 @@
 package io.dev.byteacademy.repository;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,7 @@ import io.dev.byteacademy.model.Category;
 
 @Repository
 public interface CategoryRepository extends MongoRepository<Category, String> {
+
+    List<Category> findByNameIn(Collection<String> names);
     
 }
